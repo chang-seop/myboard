dcc08a122df810195c732b5e5e168b9a191002e9
chang-seop <ikavon@naver.com>
2023-09-14 23:40:24 +0900

    INSTALL: SpringBoot
    Dependencies: Spring Web, Lombok, Mybatis, H2 Database, Thymeleaf

---
+++ pagging/src/main/java/hello/board/pagging/PaggingApplication.java
@@ -0,0 +1,13 @@
+package hello.board.pagging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaggingApplication.class, args);
+	}
+
+}
