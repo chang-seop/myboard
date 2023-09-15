fbe41e7b8c9d18bcb0dbf15300a336b070b311ec
chang-seop <ikavon@naver.com>
2023-09-15 22:13:13 +0900

    INSTALL: Spring Boot Test

---
+++ pagging/src/test/java/hello/board/pagging/PaggingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.board.pagging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
