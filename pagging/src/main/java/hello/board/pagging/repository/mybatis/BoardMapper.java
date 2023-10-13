e674d72648397392ce4ddf13d37350732d361db2
chang-seop <ikavon@naver.com>
2023-10-13 14:59:27 +0900

    MODIFY: BoardMapper, BoardMapper.xml

--- pagging/src/main/java/hello/board/pagging/repository/mybatis/BoardMapper.java
+++ pagging/src/main/java/hello/board/pagging/repository/mybatis/BoardMapper.java
@@ -1,6 +1,7 @@
 package hello.board.pagging.repository.mybatis;
 
 import hello.board.pagging.domain.Board;
+import hello.board.pagging.model.board.SearchDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,6 +13,6 @@ public interface BoardMapper {
     void save(Board board);
     Optional<Board> findById(Long id);
     List<Board> findByMemberId(Long memberId);
-    List<Board> findAll(@Param("startPosition") int startPosition, @Param("maxResult") int maxResult, @Param("title") boolean title);
+    List<Board> findAll(@Param("params") SearchDto params);
     Integer getPageMaxCount();
 }
