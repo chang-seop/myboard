caef037e49bf3b7371c0dc3dd0dd4b4c5d64f729
chang-seop <ikavon@naver.com>
2023-10-15 11:07:13 +0900

    MODIFY: Pagination

--- pagging/src/main/java/hello/board/pagging/model/Pagination.java
+++ pagging/src/main/java/hello/board/pagging/model/Pagination.java
@@ -1,96 +1,53 @@
 package hello.board.pagging.model;
 
+import hello.board.pagging.model.board.SearchDto;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class Pagination {
-    /** 1. 페이지 당 보여지는 게시글의 최대 개수 **/
-    private int pageSize = 10;
+    private int totalRecordCount; //전체 데이터 수
+    private int totalPageCount; // 전체 페이지 수
+    private int startPage; // 첫 페이지 번호
+    private int endPage; // 끝 페이지 번호
+    private int limitStart; // LIMIT 시작 위치
+    private boolean existPrevPage; // 이전 페이지 존재 여부
+    private boolean existNextPage; // 다음 페이지 존재 여부
 
-    /** 2. 페이징된 버튼의 블럭당 최대 개수 **/
-    private int blockSize = 5;
+    public Pagination(int totalRecordCount, SearchDto params) {
+        if(totalRecordCount > 0) {
+            this.totalPageCount = totalRecordCount;
+            calculation(params);
+        }
+    }
 
-    /** 3. 현재 페이지 **/
-    private int page = 1;
+    private void calculation(SearchDto params) {
+        // 전체 페이지 수 계산
+        totalPageCount = ((totalRecordCount - 1) / params.getRecordSize()) + 1;
 
-    /** 4. 현재 블럭 **/
-    private int block = 1;
-
-    /** 5. 총 게시글 수 **/
-    private int totalListCnt;
-
-    /** 6. 총 페이지 수 **/
-    private int totalPageCnt;
-
-    /** 7. 총 블럭 수 **/
-    private int totalBlockCnt;
-
-    /** 8. 블럭 시작 페이지 **/
-    private int startPage = 1;
-
-    /** 9. 블럭 마지막 페이지 **/
-    private int endPage = 1;
-
-    /** 10. DB 접근 시작 index **/
-    private int startIndex = 0;
-
-    /** 11. 이전 블럭의 마지막 페이지 **/
-    private int prevBlock;
-
-    /** 12. 다음 블럭의 시작 페이지 **/
-    private int nextBlock;
-
-    public Pagination(int totalListCnt, int page) {
-        // 총 게시물 수와 현재 페이지를 Controller 로 부터 받아온다.
-
-        // 총 게시물 수 : totalListCnt
-        // 현재 페이지 - page
-
-        // 현재 페이지
-        setPage(page);
-
-        // 총 게시글 수
-        setTotalListCnt(totalListCnt);
-
-        // 총 페이지 수
-        // 한 페이지의 최대 개수를 총 게시물 수 * 1.0로 나누어주고 올림 해준다.
-        setTotalPageCnt((int) Math.ceil(totalListCnt * 1.0 / pageSize));
-
-        // 총 블럭 수
-        // 한 블럭의 최대 개수를 총 페이지의 수 * 1.0로 나누어주고 올림 해준다.
-        setTotalBlockCnt((int) Math.ceil(totalPageCnt * 1.0 / blockSize));
-
-        // 현재 블럭
-        // 현재 페이지 * 1.0을 블럭의 최대 개수로 나누어주고 올림한다.
-        // 현재 블록을 구할 수 있다.
-        setBlock((int) Math.ceil((page * 1.0) / blockSize));
-
-        // 블럭 시작 페이지
-        setStartPage((block - 1) * blockSize + 1);
-
-        // 블럭 마지막 페이지
-        setEndPage(startPage + blockSize - 1);
-
-        // 블럭 마지막 페이지에 대한 validation
-        if(endPage > totalPageCnt) {
-            this.endPage = totalPageCnt;
+        // 현재 페이지 번호가 전체 페이지 수 보다 큰 경우, 현재 페이지 번호에 전체 페이지 수 저장
+        if(params.getPage() > totalPageCount) {
+            params.setPage(totalPageCount);
         }
 
-        // 이전 블럭 (클릭 시, 이전 블럭 마지막 페이지)
-        setPrevBlock((block * blockSize) - blockSize);
+        // 첫 페이지 번호 계산
+        startPage = ((params.getPage() - 1) / params.getPageSize()) * params.getPageSize() + 1;
 
-        // 다음 블럭(클릭 시, 다음 블럭 첫번째 페이지)
-        setNextBlock((block * blockSize) + 1);
+        // 끝 페이지 번호 계산
+        endPage = startPage + params.getPageSize() - 1;
 
-        // 다음 블럭에 대한 validation
-        if(nextBlock > totalListCnt) {
-            nextBlock = totalPageCnt;
+        // 끝 페이지가 전체 페이지 수보다 큰 경우, 끝 페이지 전체 페이지 수 저장
+        if(endPage > totalPageCount) {
+            endPage = totalPageCount;
         }
 
-        // DB 접근 시작 index
-        setStartIndex((page - 1) * pageSize);
+        // LIMIT 시작 위치 계산
+        limitStart = (params.getPage() - 1) * params.getRecordSize();
+
+        // 이전 페이지 존재 여부 확인
+        existPrevPage = startPage != 1;
+
+        // 다음 페이지 존재 여부 확인
+        existNextPage = (endPage * params.getRecordSize()) < totalRecordCount;
     }
 
 }
