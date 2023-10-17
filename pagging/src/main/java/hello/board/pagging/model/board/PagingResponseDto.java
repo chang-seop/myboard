1b23a337b71a724c51a4092f348c30ff7d77dd00
chang-seop <ikavon@naver.com>
2023-10-17 10:17:16 +0900

    ADD: PagingResponseDto

---
+++ pagging/src/main/java/hello/board/pagging/model/board/PagingResponseDto.java
@@ -0,0 +1,19 @@
+package hello.board.pagging.model.board;
+
+import hello.board.pagging.model.Pagination;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class PagingResponseDto<T> {
+    private List<T> list = new ArrayList<>();
+    private Pagination pagination;
+
+    public PagingResponseDto(List<T> list, Pagination pagination) {
+        this.list.addAll(list);
+        this.pagination = pagination;
+    }
+}
