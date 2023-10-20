bf845dda7b6b35fb130395cae17cb8d8774a533f
chang-seop <ikavon@naver.com>
2023-10-20 10:54:30 +0900

    MODIFY: FILE.sql, File

--- pagging/schema/FILE.sql
+++ pagging/schema/FILE.sql
@@ -5,7 +5,8 @@ create table file (
     board_id    bigint not null,
     upload_file_name    varchar(100) not null,
     store_file_name    varchar(100) not null,
+    file_image_yn      boolean not null,
     file_regdate      timestamp not null,
     primary key (file_id),
-    foreign key (board_id) references board (board_id)
+    foreign key (board_id) references board (board_id) on delete cascade
 );
--- pagging/src/main/java/hello/board/pagging/domain/File.java
+++ pagging/src/main/java/hello/board/pagging/domain/File.java
@@ -14,14 +14,16 @@ public class File {
     private Long boardId;
     private String uploadFileName;
     private String storeFileName;
+    private Boolean fileImageYn;
     private LocalDateTime fileRegdate;
 
     @Builder
-    public File(Long fileId, Long boardId, String uploadFileName, String storeFileName, LocalDateTime fileRegdate) {
+    public File(Long fileId, Long boardId, String uploadFileName, String storeFileName, Boolean fileImageYn, LocalDateTime fileRegdate) {
         this.fileId = fileId;
         this.boardId = boardId;
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
+        this.fileImageYn = fileImageYn;
         this.fileRegdate = fileRegdate;
     }
 }
