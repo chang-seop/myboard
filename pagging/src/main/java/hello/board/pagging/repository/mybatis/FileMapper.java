900590f43c67448069de0b9ab4b9f4a69a412cf8
chang-seop <ikavon@naver.com>
2023-10-20 10:55:10 +0900

    MODIFY: FileMapper, FileMapper.xml

--- pagging/src/main/java/hello/board/pagging/repository/mybatis/FileMapper.java
+++ pagging/src/main/java/hello/board/pagging/repository/mybatis/FileMapper.java
@@ -2,6 +2,7 @@ package hello.board.pagging.repository.mybatis;
 
 import hello.board.pagging.domain.File;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,6 @@ public interface FileMapper {
     void saveAll(List<File> files);
     Optional<File> findById(Long fileId);
     List<File> findByBoardId(Long boardId);
+    List<File> findByBoardIdAndFileImageYn(@Param("boardId") Long boardId, @Param("fileImageYn") Boolean fileImageYn); // true == 이미지파일, false == 첨부파일
+    Optional<File> findByStoreFileName(String storeFileName);
 }
