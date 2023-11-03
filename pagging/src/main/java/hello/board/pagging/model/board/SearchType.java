dd926fc47d3b8083790ed37297d9982ec172a1cf
chang-seop <ikavon@naver.com>
2023-11-03 19:03:20 +0900

    ADD: SearchType

---
+++ pagging/src/main/java/hello/board/pagging/model/board/SearchType.java
@@ -0,0 +1,22 @@
+package hello.board.pagging.model.board;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public enum SearchType {
+    content("제목", "title"),
+    writer("글쓴이", "writer"),
+
+    ; // End
+
+    private String name;
+    private String value;
+
+    SearchType(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
